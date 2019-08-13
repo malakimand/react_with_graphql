607784775642df0d29e2b77d76ba3a9af009e5c5
malakimand <malakimand@gmail.com>
2019-08-12 22:49:32 -0700

    first update

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://root:root123@ds261817.mlab.com:61817/gql-jim');
+mongoose.connection.once('open', () => {
+	console.log('connected to database');
+})
+
+
+app.use('/graphql', graphqlHTTP({
+	schema,
+	graphiql: true
+}));
+
+app.listen(4000, () => {
+	console.log('now listening for requests on port 4000');
+});
